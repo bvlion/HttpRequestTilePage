42d72a1fbb2682cce9831719c0191cef4e354168
bvlion <bvltiggeari@ambitious-i.net>
2023-07-29 11:54:07 +0900

    export dark $ light 4 mobile

--- parse.js
+++ parse.js
@@ -6,13 +6,15 @@ marked.setOptions({
 })
 
 const argv = process.argv.slice(2)
-const title = argv[0]
+const dir = argv[0]
 const name = argv[1]
+const css = argv[2]
+const title = argv[3]
 
 const markdown = fs.readFileSync(`docs/${name}.md`, 'utf8')
 const base = fs.readFileSync('docs/rules.html', 'utf8')
 
 const html = marked.parse(markdown)
-const result = base.replace(/replace_body/g, html).replace(/replace_title/g, title)
+const result = base.replace(/replace_body/g, html).replace(/replace_title/g, title).replace(/replace_css/g, css)
 
-fs.writeFileSync(`public/${name}.html`, result)
+fs.writeFileSync(`public${dir}${name}.html`, result)
